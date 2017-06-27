57039ce55fba3b780aa9bedccea8edaa214031df
iTwicKz <twickes32@ufl.edu>
2017-06-27 11:13:00 -0700

    Fun rotating

--- assets/js/cog.js
+++ assets/js/cog.js
@@ -1,9 +1,29 @@
-var $cog = $('#cog'),
-    $body = $(document.body),
-    bodyHeight = $body.height();
+$(document).ready(function(){
+   var bodyHeight = $(window).height();
+   window.onscroll = function() {
+     console.log("Leftside");
+      //Determine the amount to rotate by.
+      var deg = -window.scrollY/1.7*(360/bodyHeight);
+      $("#cog").css({
+        "transform": "rotate("+deg+"deg)",
+      });
+      $("#cog-right").css({
+        "transform": "rotate("+-deg+"deg)",
+      });
 
-$(window).scroll(function () {
-    $cog.css({
-        'transform': 'rotate(' + ($body.scrollTop() / bodyHeight * 360) + 'deg)'
-    });
+   };
 });
+
+// $(document).ready(function(){
+//    var bodyHeight = $(window).height();
+//    window.onscroll = function() {
+//      console.log("Rightside");
+//
+//       //Determine the amount to rotate by.
+//       var deg = +window.scrollY/1.7*(360/bodyHeight);
+//       $("#cog-right").css({
+//         "transform": "rotate("+deg+"deg)",
+//       });
+//
+//    };
+// });
