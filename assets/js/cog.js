46717201c94a184740bda8baf9d0359f644e4ad1
iTwicKz <twickes32@ufl.edu>
2017-06-27 21:22:07 -0700

    Playing more get ready Nikhil

--- assets/js/cog.js
+++ assets/js/cog.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
    var bodyHeight = $(window).height();
    var top = true;
    var deg = 0;
+   var page = 1;
+   $('#click1').hide();
    window.onscroll = function() {
      if (window.scrollY < bodyHeight*.75) {
        console.log("It worked here" + (window.scrollY-bodyHeight*.75));
@@ -11,11 +13,16 @@ $(document).ready(function(){
        $("#cog-right").css({
         "margin-right": ""+ window.scrollY-bodyHeight*.75 +"px",
        });
+      $('#click1').hide();
        deg = 0;
      }
     //  console.log("Leftside");
       //Determine the amount to rotate by.
       // console.log(1+bodyHeight/980);
+      if(window.scrollY >= (bodyHeight*.75*page)-210){
+        page += 1;
+        $('#click1').show();
+      }
       if(window.scrollY >= bodyHeight*.75){
         deg = -(window.scrollY-bodyHeight*0.75)/(1+bodyHeight/1000)*(360/bodyHeight);
         // if (window.scrollY < bodyHeight) {
